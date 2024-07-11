57f8d8734079bef326f9b55696d54c0e2cba1713
youda <youda0518@yahoo.co.jp>
2024-07-11 14:41:04 +0900

    実装完了

--- app/stage1/client.py
+++ app/stage1/client.py
@@ -1,5 +1,9 @@
 import socket
 import sys
+import os
+
+def protocol_header(filename_length, data_length):
+    return filename_length.to_bytes(1, "big") + data_length.to_bytes(32, "big")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -15,7 +19,42 @@ except socket.error as err:
     sys.exit(1)
 
 try:
-    filepath = input('Type in a file to upload: ')
+    filepath = input('アップロードするファイルを入力してください: ')
+
+    # バイナリモードでファイルを読み込む
+    with open(filepath, 'rb') as f:
+
+        # mp４でないファイルは弾く
+        file_header = f.read(12)
+        if b'ftyp' in file_header:
+
+            # ファイルサイズの算出
+            f.seek(0, os.SEEK_END)
+            file_size = f.tell()
+            f.seek(0, 0)
+
+            if file_size > pow(2, 32):
+                raise Exception('ファイルは4GB以下にしてください')
+
+            filename = os.path.basename(f.name)
+            filename_bits = filename.encode('utf-8')
+
+            header = protocol_header(len(filename_bits), file_size)
+
+            # ヘッダーの送信
+            sock.send(header)
+
+            # ファイル名の送信
+            sock.send(filename_bits)
+
+            # 一度に1460バイトずつ読み出し、送信する
+            data = f.read(1460)
+            while data:
+                print("Sending...")
+                sock.send(data)
+                data = f.read(1460)
+        else:
+            print('ファイルがmp4形式ではありません')
 finally:
     print('closing socket')
     sock.close()
--- app/stage1/server.py
+++ app/stage1/server.py
@@ -22,8 +22,46 @@ while True:
 
     try:
         print('connection from', client_address)
+
+        # ヘッダーの読み取り
+        header = connection.recv(33)
+
+        filename_length = int.from_bytes(header[:1], "big")
+        data_length = int.from_bytes(header[1:33], "big")
+        stream_rate = 1460
+
+        print(header)
+
+        print('Recieved header from client. Byte length: Title length {}, Data Length {}'.format(filename_length, data_length))
+
+        filename = connection.recv(filename_length).decode('utf-8')
+
+        print('Filename: {}'.format(filename))
+
+        # 読み取ったデータがないときは例外処理
+        if data_length == 0:
+            raise Exception('No data to read from client.')
+        
+        
+
+        # クライアントから受け取ったファイルをtemp配下にコピーする
+        with open(os.path.join(dpath, filename), 'wb+') as f:
+            while data_length > 0:
+                data = connection.recv(data_length if data_length <= stream_rate else stream_rate)
+                f.write(data)
+                print('received {} bytes'.format(len(data)))
+                data_length -= len(data)
+                print(data_length)
+
+        print('Finished downloading the file from client')
+        message = "OK : ファイルを正常に受信しました。"
+        connection.sendall(message.encode('utf-8'))
+
     except Exception as e:
         print('Error: ' + str(e))
+        message = "NG : ファイルの受信に失敗しました"
+        connection.sendall(message.encode('utf-8'))
+
     finally:
         print("Closing current connection")
         connection.close()
